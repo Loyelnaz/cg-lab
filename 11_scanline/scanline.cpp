3643583819d1f861f919e7128cecf23037c1bc83
Loyelnaz <nazloyel59@gmail.com>
2016-11-29 14:21:57 +0530

    scanline area fill code

---
+++ 11_scanline/scanline.cpp
@@ -0,0 +1,124 @@
+#ifdef __APPLE__
+
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+#else
+
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+#endif
+
+#include <iostream>
+using namespace std;
+
+float x1 = 200, y1 = 200, x2 = 100, y2 = 300, x3 = 200, y3 = 400, x4 = 300, y4 = 300;
+
+void draw_pixel(int x, int y) {
+    glPointSize(1.0);
+    glBegin(GL_POINTS);
+        glVertex2i(x, y);
+    glEnd();
+}
+
+void edgeDetect(float x1, float y1, float x2, float y2, int *left_edge, int *right_edge) {
+    float temp, mx, x;
+
+    if(y1 > y2) {
+        temp = x1; x1 = x2; x2 = temp;
+        temp = y1; y1 = y2; y2 = temp;
+    }
+
+    if(y1 == y2)
+        mx = x2 - x1;
+    else
+        mx = (x2 - x1) / (y2 - y1);
+
+    x = x1;
+
+    for(int i = y1; i <= y2; i++) {
+        if(x < left_edge[i])
+            left_edge[i] = x;
+        if(x > right_edge[i])
+            right_edge[i] = x;
+
+        x += mx;
+    }
+}
+
+void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+    int left_edge[500], right_edge[500];
+    for(int i = 0; i < 500; i++) {
+        left_edge[i] = 500;
+        right_edge[i] = 0;
+    }
+
+    edgeDetect(x1, y1, x2, y2, left_edge, right_edge);
+    edgeDetect(x2, y2, x3, y3, left_edge, right_edge);
+    edgeDetect(x3, y3, x4, y4, left_edge, right_edge);
+    edgeDetect(x4, y4, x1, y1, left_edge, right_edge);
+
+    for(int y = 0; y < 500; y++) {
+        if(left_edge[y] <= right_edge[y]) {
+            for(int i = left_edge[y]; i < right_edge[y]; i++) {
+                glFlush();
+                draw_pixel(i, y);
+            }
+        }
+    }
+}
+
+void myInit() {
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D(0, 500, 0, 500);
+}
+
+void scanfillDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(2.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y2);
+        glVertex2f(x3, y3);
+        glVertex2f(x4, y4);
+    glEnd();
+
+    glColor3f(0.0, 0.0, 1.0);
+    scanfill(x1, y1, x2, y2, x3, y3, x4, y4);
+    glFlush();
+}
+
+void myDisplay() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLineWidth(2.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y2);
+        glVertex2f(x3, y3);
+        glVertex2f(x4, y4);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Original");
+    myInit();
+    glutDisplayFunc(myDisplay);
+
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(500, 0);
+    glutCreateWindow("Scanfill");
+    myInit();
+    glutDisplayFunc(scanfillDisplay);
+    glutMainLoop();
+    return 0;
+}
